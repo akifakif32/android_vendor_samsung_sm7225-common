6cfe73b9c7cd7592372ccdae574ececc1f41ff97
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-18 01:11:11 +0300

    sm7225-common: Sync with device tree

--- sm7225-common-vendor.mk
+++ sm7225-common-vendor.mk
@@ -308,6 +308,7 @@ PRODUCT_PACKAGES += \
     libswdap \
     libswspatializer \
     libvisualizer \
+    spatializer-aidl-V1-cpp \
     unnhal-acc-hta \
     vendor.qti.gnss@1.0 \
     vendor.qti.gnss@1.1 \
@@ -371,7 +372,6 @@ PRODUCT_PACKAGES += \
     libwvdrmengine \
     libqcompostprocbundle \
     libvolumelistener \
-    spatializer-aidl-V1-cpp \
     vendor.qti.hardware.audiohalext@1.0 \
     camera.unihal.default \
     gatekeeper.mdfpp \
